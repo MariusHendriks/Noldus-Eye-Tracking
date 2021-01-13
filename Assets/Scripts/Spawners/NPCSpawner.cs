d6ec3837237b02255ecbad8ab9a8c409bf099e7d
Jop Verhoeven <jop.verhoeven@gmail.com>
2021-01-13 16:12:35 +0100

    Added spawners, random pathfinding, car models

---
+++ Assets/Scripts/Spawners/NPCSpawner.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCSpawner : MonoBehaviour
+{
+    public GameObject[] NPCPrefabs;
+    public GameObject[] pathPrefabs;
+    private GameObject[] pathObjects;
+
+
+    public int maxNPCSpawn = 30;
+    public int spawnTimeMin = 1;
+    public int spawnTimeMax = 20;
+
+    private int currentNPCSpawn = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pathObjects = new GameObject[pathPrefabs.Length];
+        for (int i = 0; i < pathPrefabs.Length; i++)
+        {
+            pathObjects[i] = Instantiate(pathPrefabs[i], new Vector3(0, 0, 0), Quaternion.identity);
+        }
+
+        StartCoroutine(SpawnNPC());
+    }
+
+    private IEnumerator SpawnNPC()
+    {
+        
+
+        GameObject npcPrefab = NPCPrefabs[Random.Range(0, NPCPrefabs.Length)];
+
+        GameObject path = pathObjects[Random.Range(0, pathObjects.Length)];
+        GameObject npc = Instantiate(npcPrefab, this.transform.position, Quaternion.identity);
+
+        PathMovement pathMovement = npc.GetComponent<PathMovement>();
+        pathMovement.pointsParent = path;
+
+        currentNPCSpawn++;
+
+        yield return new WaitForSeconds(Random.Range(spawnTimeMin, spawnTimeMax));
+
+        if (currentNPCSpawn >= maxNPCSpawn){
+            StopCoroutine(SpawnNPC());
+
+        }
+        else {
+            StartCoroutine(SpawnNPC());
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Traffic/CarPathMovement.cs
@@ -0,0 +1,82 @@
+﻿// Patrol.cs
+using UnityEngine;
+using UnityEngine.AI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CarPathMovement : MonoBehaviour
+{
+    public GameObject pointsParent;
+    public List<Transform> points;
+
+    [Range(0.2f, 1.0f)]
+    public float distanceToPoint = 0.2f;
+
+    public bool resetOnLastWaypoint = false;
+
+    private int destPoint = 0;
+    private NavMeshAgent agent;
+
+    private int collisions = 0;
+
+    public CarCollisionDetection frontDetection;
+    public CarCollisionDetection midDetection;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+
+        if (pointsParent != null)
+        {
+            foreach (Transform child in pointsParent.transform)
+            {
+                this.points.Add(child);
+            }
+        }
+
+
+        agent.autoBraking = false;
+
+        GotoNextPoint();
+    }
+
+    void GotoNextPoint()
+    {
+        // Returns if no points have been set up
+        if (points.Count == 0)
+            return;
+
+        if (resetOnLastWaypoint && destPoint == points.Count)
+        {
+            Destroy(this.gameObject);
+            Destroy(this);
+            return;
+        }
+
+
+        // Set the agent to go to the currently selected destination.
+        agent.destination = points[destPoint].position;
+
+        // Choose the next point in the array as the destination,
+        // cycling to the start if necessary.
+        destPoint++;
+
+       
+    }
+
+    void Update()
+    {
+        if (frontDetection.isColliding)
+        {
+            agent.speed = 0;
+        } else
+        {
+            agent.speed = 5f;
+        }
+
+        // Choose the next destination point when the agent gets
+        // close to the current one.
+        if (!agent.pathPending && agent.remainingDistance < distanceToPoint)
+            GotoNextPoint();
+    }
+}

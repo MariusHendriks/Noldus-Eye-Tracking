c4ca059e2021de28d0dccfd55c5207465dede2ea
MariusHendriks <mark.hendriks9999@gmail.com>
2021-01-21 13:11:16 +0100

    Added tons of static people for ambiance

---
+++ Assets/Scripts/GoForward.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoForward : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x + Time.deltaTime * 50f, transform.position.y, transform.position.z);
+        if (transform.position.x > 1900f)
+        {
+            Object.Destroy(this.gameObject);
+        }
+    }
+}

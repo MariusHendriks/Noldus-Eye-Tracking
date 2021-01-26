0f167620d12b05c4f44ba957e1e866c0e8b5b3bd
Hristov, Dimitar <mimaqm@gmail.com>
2021-01-26 17:53:11 +0100

    After merge fixes

--- Assets/Scripts/ObjectMovement/RotationAroundOwnAxis.cs
+++ Assets/Scripts/ObjectMovement/RotationAroundOwnAxis.cs
@@ -25,7 +25,6 @@ public class RotationAroundOwnAxis : MonoBehaviour
         transform.RotateAround(transform.position, Vector3.right, SpeedX * Time.deltaTime);
         transform.RotateAround(transform.position, Vector3.up, SpeedY * Time.deltaTime);
         transform.RotateAround(transform.position, Vector3.forward, SpeedZ * Time.deltaTime);
-
     }
 
     public void Randomize(int MeshSet)
@@ -35,8 +34,25 @@ public class RotationAroundOwnAxis : MonoBehaviour
         SpeedZ = Random.Range(-200, 200);
         MeshFilter mesh = this.gameObject.GetComponent<MeshFilter>();
         mesh.sharedMesh = meshes[Random.Range((MeshSet*4), (MeshSet+1)*4)];
+        this.gameObject.GetComponent<MeshCollider>().sharedMesh = mesh.sharedMesh;
+        this.gameObject.GetComponent<MeshCollider>().convex = true;
+        this.gameObject.GetComponent<MeshCollider>().isTrigger = true;
+
         GetComponent<Renderer>().material.SetFloat("_Metallic", 0.5f);
         GetComponent<Renderer>().material.SetFloat("_Glossiness", 0.5f);
         GetComponent<Renderer>().material.color = new Color(Random.Range(0F, 1F), Random.Range(0, 1F), Random.Range(0, 1F));
+    }
+
+    public void HighlightObject(bool highlight)
+    {
+        if (highlight)
+        {
+            GetComponent<Renderer>().material.EnableKeyword("_EMISSION");
+            GetComponent<Renderer>().material.SetColor("_EmissionColor", Color.green);
+        }
+        else
+        {
+            GetComponent<Renderer>().material.DisableKeyword("_EMISSION");
+        }
     }
 }

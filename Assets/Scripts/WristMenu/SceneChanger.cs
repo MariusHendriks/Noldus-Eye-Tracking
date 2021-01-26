7951c0ed97b392d422ad1eaa7015dad5b9333881
Hristov, Dimitar <mimaqm@gmail.com>
2021-01-26 16:14:09 +0100

    Wrist Menu Final touches + folder rearrangement

---
+++ Assets/Scripts/WristMenu/SceneChanger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public void ChangeToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void ChangeToTrafficScene()
+    {
+        SceneManager.LoadScene("TrafficScene");
+    }
+
+    public void ChangeToBrownian()
+    {
+        SceneManager.LoadScene("Brownian");
+    }
+}

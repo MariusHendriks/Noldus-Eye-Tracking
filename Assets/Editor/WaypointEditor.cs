a2e1994167c83721b04e3939463a8c6a54b528e6
CrunchyPenguin <36261122+CrunchyPenguin@users.noreply.github.com>
2021-01-28 22:02:49 +0100

    waypoint gizmos only visible when path is selected

--- Assets/Editor/WaypointEditor.cs
+++ Assets/Editor/WaypointEditor.cs
@@ -6,38 +6,41 @@ using UnityEditor;
 [InitializeOnLoad()]
 public class WaypointEditor
 {
-    [DrawGizmo(GizmoType.NonSelected | GizmoType.Selected | GizmoType.Pickable)]
+    [DrawGizmo(GizmoType.InSelectionHierarchy | GizmoType.Selected | GizmoType.Pickable)]
     public static void OnDrawSceneGizmos(Waypoint waypoint, GizmoType gizmoType)
     {
-        if ((gizmoType & GizmoType.Selected) != 0)
+        if ((gizmoType & GizmoType.InSelectionHierarchy) != 0)
         {
-            Gizmos.color = Color.yellow;
-        }
-        else
-        {
-            Gizmos.color = Color.yellow * 0.5f;
-        }
+            if ((gizmoType & GizmoType.Selected) != 0)
+            {
+                Gizmos.color = Color.yellow;
+            }
+            else
+            {
+                Gizmos.color = Color.yellow * 0.5f;
+            }
 
-        Gizmos.DrawSphere(waypoint.transform.position, .1f);
+            Gizmos.DrawSphere(waypoint.transform.position, .1f);
 
-        Gizmos.color = Color.white;
-        Gizmos.DrawLine(waypoint.transform.position + (waypoint.transform.right * waypoint.width / 2f), waypoint.transform.position - (waypoint.transform.right * waypoint.width / 2f));
+            Gizmos.color = Color.white;
+            Gizmos.DrawLine(waypoint.transform.position + (waypoint.transform.right * waypoint.width / 2f), waypoint.transform.position - (waypoint.transform.right * waypoint.width / 2f));
 
-        if (waypoint.previousWaypoint != null)
-        {
-            Gizmos.color = Color.red;
-            Vector3 offset = waypoint.transform.right * waypoint.width / 2f;
-            Vector3 offsetTo = waypoint.previousWaypoint.transform.right * waypoint.previousWaypoint.width / 2f;
+            if (waypoint.previousWaypoint != null)
+            {
+                Gizmos.color = Color.red;
+                Vector3 offset = waypoint.transform.right * waypoint.width / 2f;
+                Vector3 offsetTo = waypoint.previousWaypoint.transform.right * waypoint.previousWaypoint.width / 2f;
 
-            Gizmos.DrawLine(waypoint.transform.position + offset, waypoint.previousWaypoint.transform.position + offsetTo);
-        }
-        if (waypoint.nextWaypoint != null && waypoint.previousWaypoint != null)
-        {
-            Gizmos.color = Color.green;
-            Vector3 offset = waypoint.transform.right * -waypoint.width / 2f;
-            Vector3 offsetTo = waypoint.nextWaypoint.transform.right * -waypoint.previousWaypoint.width / 2f;
+                Gizmos.DrawLine(waypoint.transform.position + offset, waypoint.previousWaypoint.transform.position + offsetTo);
+            }
+            if (waypoint.nextWaypoint != null && waypoint.previousWaypoint != null)
+            {
+                Gizmos.color = Color.green;
+                Vector3 offset = waypoint.transform.right * -waypoint.width / 2f;
+                Vector3 offsetTo = waypoint.nextWaypoint.transform.right * -waypoint.previousWaypoint.width / 2f;
 
-            Gizmos.DrawLine(waypoint.transform.position + offset, waypoint.nextWaypoint.transform.position + offsetTo);
+                Gizmos.DrawLine(waypoint.transform.position + offset, waypoint.nextWaypoint.transform.position + offsetTo);
+            }
         }
     }
 }
